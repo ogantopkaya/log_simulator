4a7a19e7ad5a352a3c60757085298693778e5808
Ogan Topkaya <otopkaya@me.com>
2014-11-02 01:24:59 +0200

    Adds bin file Adds parser and socket comm.

--- lib/log_simulator.rb
+++ lib/log_simulator.rb
@@ -1,7 +1,37 @@
-require "log_simulator/version"
 
 module LogSimulator
-  def self.greet
-    puts 'greet'
+  class PlusLogSimulator
+
+    def self.start(filepath)
+      begin
+        socket = TCPSocket.new 'localhost',7658
+      rescue Exception => _
+        socket = nil
+      end
+
+      path = File.expand_path(filepath)
+      puts "Opening log file at path: " + path
+
+      time = 0
+      timescale = 1
+
+      File.open(path,'r+:utf-8') do |file|
+        file.each_line do |line|
+          line.scan(/N\|(\d+)\|RECEIVE << (.*)/) do |timeStr,message|
+            _time = timeStr.to_i
+            if time != 0
+              sleep (_time - time) * timescale
+            end
+            if socket != nil
+              socket.puts message
+            end
+            puts '<' + message
+            time = _time
+          end
+        end
+      end
+
+    end
+
   end
 end
